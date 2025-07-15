80c2d75251659a0c77d457bffe0abb7fa8dab63c
raschedh <rasched1997@gmail.com>
2025-07-15 15:15:28 +0100

    fixed model save dir

--- utils.py
+++ utils.py
@@ -7,28 +7,30 @@ def load_model(save_dir):
     model = AutoModelForCausalLM.from_pretrained(save_dir)
     return model, tokenizer
 
-def download_model(model_dir):
-    os.makedirs(model_dir, exist_ok=True)
+def download_model(model_name, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
 
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
-    model = AutoModelForCausalLM.from_pretrained(model_dir)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForCausalLM.from_pretrained(model_name)
 
-    model.save_pretrained(model_dir)
-    tokenizer.save_pretrained(model_dir)
-    print(f"✅ Model and tokenizer saved to: {model_dir}")
+    model.save_pretrained(save_dir)
+    tokenizer.save_pretrained(save_dir)
+    print(f"✅ Model and tokenizer saved to: {save_dir}")
     return 
 
-def download_dataset(data_dir):
-    os.makedirs(data_dir, exist_ok=True)
-    ds = load_dataset(data_dir)
-    ds.save_to_disk(data_dir)
-    print(f"✅ Dataset saved locally at: {data_dir}")
-    return 
+def download_dataset(dataset_name, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
+    ds = load_dataset(dataset_name)
+    ds.save_to_disk(save_dir)
+    print(f"✅ Dataset saved locally at: {save_dir}")
+    return
+
 
 if __name__ == "__main__":
-    download_model(model_dir="ThinkPRM-7B")
-    download_model(model_dir="Qwen/Qwen2.5-7B")
+    download_model(model_name="launch/ThinkPRM-7B", save_dir="ThinkPRM-7B")
+    download_model(model_name="Qwen/Qwen2.5-7B", save_dir="Qwen2.5-7B")
     
-    download_dataset(data_dir="Asap7772/cog_behav_all_strategies")
-    download_dataset(data_dir="Jiayi-Pan/Countdown-Tasks-3to4")
+    download_dataset("Asap7772/cog_behav_all_strategies", "cog_behav_all_strategies")
+    download_dataset("Jiayi-Pan/Countdown-Tasks-3to4", "Countdown-Tasks-3to4")
+
 
